b874c4c3c9e44b7657660d67163f1ca7dfa36c74
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2023-07-31 22:49:42 +0300

    Add files via upload

---
+++ HastaneRandevuSistemi/Controllers/DoktorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class DoktorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
