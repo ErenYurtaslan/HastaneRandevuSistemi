f1943c969069ac8b3075918a39833ace1956c5b4
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2023-07-31 22:44:02 +0300

    Add files via upload

---
+++ HastaneRandevuSistemi/Controllers/Controllers/KiralamaController.cs
@@ -0,0 +1,196 @@
+﻿using asp_udemy_proje1.Models;
+using asp_udemy_proje1.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+using System.Linq;
+
+namespace asp_udemy_proje1.Controllers
+{
+    [Authorize(Roles = UserRoles.Role_Admin)]//bütün controller dosyasında auth istersen buraya,
+                                             //spesifik actionlar için istersen aşağıda istediğin metodun başına bunu yazıyorsun 
+    public class KiralamaController : Controller
+    {
+
+        private readonly IKiralamaRepository _kiralamaRepository;
+        private readonly IKitapRepository _kitapRepository;
+        public readonly IWebHostEnvironment _webHostEnvironment;
+
+        public KiralamaController(IKiralamaRepository kiralamaRepository, IKitapRepository kitapRepository, IWebHostEnvironment webHostEnvironment)
+        {
+            _kiralamaRepository = kiralamaRepository;
+            _kitapRepository = kitapRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+           // List<Kitap> objKitapList = _kitapRepository.GetAll().ToList();
+
+            List<Kiralama> objKiralamaList = _kiralamaRepository.GetAll(includeProps:"Kitap").ToList();
+
+            return View(objKiralamaList);
+        }
+
+
+
+
+        public IActionResult EkleGuncelle(int? id)
+        {
+            //List<Kitap> objKitapList = _uygulamaDbContext.Kitaplar.ToList();
+
+            IEnumerable<SelectListItem> KitapList = _kitapRepository.GetAll().
+                Select(
+                k => new SelectListItem
+                {
+                    Text = k.KitapAdi,
+                    Value = k.Id.ToString(),
+                }
+               );
+
+            ViewBag.KitapList = KitapList;
+
+
+            if (id == null || id == 0)//ekle
+            {
+
+                return View();
+
+            }
+            else{//güncelle
+
+                 // Kitap? kitapDb =  _uygulamaDbContext.Kitaplar.Find(id); eski kullanım
+
+                Kiralama? kiralamaDb = _kiralamaRepository.Get(u => u.Id == id);//Expression<Func<T, bool>> filtre kısmı
+
+                if (kiralamaDb == null)
+                {
+
+
+                    return NotFound();
+
+                }
+                return View(kiralamaDb);
+            }
+        }
+
+
+
+
+        [HttpPost]//bunu yazmazsan yukarıda aynı isimle action olduğu için "catch" çalışır, sayfayı göremezsin.
+        public IActionResult EkleGuncelle(Kiralama kiralama)
+        {
+
+            if(ModelState.IsValid) {
+
+                
+
+
+                if (kiralama.Id == 0) {
+                    _kiralamaRepository.Ekle(kiralama);
+                    TempData["basarili"] = "Yeni kiralama kaydedildi!";
+                }
+                else {
+                    _kiralamaRepository.Guncelle(kiralama);
+                    TempData["basarili"] = "Kiralama güncellendi!";
+                }
+
+                _kiralamaRepository.Kaydet();//bunu yapmazsan db'ye bilgiler eklenmez.
+                
+                
+               
+                return RedirectToAction("Index", "Kiralama");//kaydolunca seni Kitap kontrolörünün index action metodunun viewine gönderir.
+            }
+            else
+            {
+                return View();
+            }
+            
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Sil(int? id)
+        {
+
+            IEnumerable<SelectListItem> KitapList = _kitapRepository.GetAll().
+                Select(
+                k => new SelectListItem
+                {
+                    Text = k.KitapAdi,
+                    Value = k.Id.ToString(),
+                }
+               );
+
+            ViewBag.KitapList = KitapList;
+
+
+            if (id == null || id == 0)
+            {
+
+                return NotFound();
+
+            }
+
+
+
+            Kiralama? kiralamaDb = _kiralamaRepository.Get(u => u.Id == id);//Expression<Func<T, bool>> filtre kısmı
+
+            if (kiralamaDb == null)
+            {
+
+
+                return NotFound();
+
+            }
+
+
+            return View(kiralamaDb);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpPost, ActionName("Sil")]//bunu yazmazsan yukarıda aynı isimle action olduğu için "catch" çalışır, sayfayı göremezsin.
+        public IActionResult SilPOST(int? id)
+        {
+
+
+            Kiralama? kiralama = _kiralamaRepository.Get(u => u.Id == id);//Expression<Func<T, bool>> filtre kısmı
+
+            if (kiralama == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _kiralamaRepository.Sil(kiralama);
+                _kiralamaRepository.Kaydet();
+                TempData["basarili"] = "Kiralama silindi!";
+                return RedirectToAction("Index", "Kiralama");//güncelleyince seni Kitap kontrolörünün index action metodunun viewine gönderir.
+            }
+        }
+
+    }
+}
---
+++ HastaneRandevuSistemi/Controllers/Controllers/KitapTuruController.cs
@@ -0,0 +1,215 @@
+﻿using asp_udemy_proje1.Models;
+using Microsoft.AspNetCore.Authorization;
+using asp_udemy_proje1.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Linq;
+
+namespace asp_udemy_proje1.Controllers
+{
+    [Authorize(Roles = UserRoles.Role_Admin)]//bütün controller dosyasında auth istersen buraya,
+                                             //spesifik actionlar için istersen aşağıda istediğin metodun başına bunu yazıyorsun 
+    public class KitapTuruController : Controller
+    {
+
+        private readonly IKitapTuruRepository _kitapTuruRepository;
+
+        public KitapTuruController(IKitapTuruRepository context)
+        {
+            _kitapTuruRepository = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<KitapTuru> objKitapTuruList = _kitapTuruRepository.GetAll().ToList();
+
+            return View(objKitapTuruList);
+        }
+
+
+
+
+        public IActionResult Ekle()
+        {
+            //List<KitapTuru> objKitapTuruList = _uygulamaDbContext.KitapTurleri.ToList();
+
+            return View();
+        }
+
+
+
+
+        [HttpPost]//bunu yazmazsan yukarıda aynı isimle action olduğu için "catch" çalışır, sayfayı göremezsin.
+        public IActionResult Ekle(KitapTuru kitapTuruEkle)
+        {
+            if(ModelState.IsValid) {
+                _kitapTuruRepository.Ekle(kitapTuruEkle);
+                _kitapTuruRepository.Kaydet();//bunu yapmazsan db'ye bilgiler eklenmez.
+                
+                    TempData["basarili"] = "Kitap Türü ekleme işlemi başarılı!";
+               
+                return RedirectToAction("Index", "KitapTuru");//kaydolunca seni KitapTuru kontrolörünün index action metodunun viewine gönderir.
+            }
+            else
+            {
+                return View();
+            }
+            
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Guncelle(int? id)
+        {
+
+
+            if (id == null || id == 0){ 
+            
+                return NotFound();
+            
+            }
+
+
+
+            // KitapTuru? kitapTuruDb = _uygulamaDbContext.KitapTurleri.Find(id); eski kullanım
+
+            KitapTuru? kitapTuruDb = _kitapTuruRepository.Get(u=>u.Id == id);//Expression<Func<T, bool>> filtre kısmı
+
+            if (kitapTuruDb == null){
+            
+
+                return NotFound();
+
+            }
+
+
+            return View(kitapTuruDb);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpPost]//bunu yazmazsan yukarıda aynı isimle action olduğu için "catch" çalışır, sayfayı göremezsin.
+        public IActionResult Guncelle(KitapTuru kitapTuruGuncelle)   
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _kitapTuruRepository.Guncelle(kitapTuruGuncelle);
+                _kitapTuruRepository.Kaydet();
+               
+                    TempData["basarili"] = "Kitap Türü güncelleme işlemi başarılı!";
+                
+                return RedirectToAction("Index", "KitapTuru");//güncelleyince seni KitapTuru kontrolörünün index action metodunun viewine gönderir.
+            }
+            else{
+
+                return View();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Sil(int? id)
+        {
+
+
+            if (id == null || id == 0)
+            {
+
+                return NotFound();
+
+            }
+
+
+
+            KitapTuru? kitapTuruDb = _kitapTuruRepository.Get(u => u.Id == id);//Expression<Func<T, bool>> filtre kısmı
+
+            if (kitapTuruDb == null)
+            {
+
+
+                return NotFound();
+
+            }
+
+
+            return View(kitapTuruDb);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpPost, ActionName("Sil")]//bunu yazmazsan yukarıda aynı isimle action olduğu için "catch" çalışır, sayfayı göremezsin.
+        public IActionResult SilPOST(int? id)
+        {
+
+
+            KitapTuru? kitapTuru = _kitapTuruRepository.Get(u => u.Id == id);//Expression<Func<T, bool>> filtre kısmı
+
+            if (kitapTuru == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _kitapTuruRepository.Sil(kitapTuru);
+                _kitapTuruRepository.Kaydet();
+                TempData["basarili"] = "Kitap Türü silme işlemi başarılı!";
+                return RedirectToAction("Index", "KitapTuru");//güncelleyince seni KitapTuru kontrolörünün index action metodunun viewine gönderir.
+            }
+        }
+
+    }
+}
