7661068064e249bff617aab466b015fa4af0dd44
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2023-08-07 23:49:53 +0300

    Add files via upload

--- HastaneRandevuSistemi/Migrations/20230807201418_ingilizceSutunEklendi.Designer.cs
+++ HastaneRandevuSistemi/Migrations/20230807201418_ingilizceSutunEklendi.Designer.cs
@@ -4,6 +4,7 @@ using HastaneRandevuSistemi.Utility;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HastaneRandevuSistemi.Migrations
 {
     [DbContext(typeof(UygulamaDbContext))]
-    partial class UygulamaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230807201418_ingilizceSutunEklendi")]
+    partial class ingilizceSutunEklendi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HastaneRandevuSistemi/Migrations/20230807201418_ingilizceSutunEklendi.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HastaneRandevuSistemi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ingilizceSutunEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CalismaEng",
+                table: "Doktorlar",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdEng",
+                table: "DoktorBranslari",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CalismaEng",
+                table: "Doktorlar");
+
+            migrationBuilder.DropColumn(
+                name: "AdEng",
+                table: "DoktorBranslari");
+        }
+    }
+}
